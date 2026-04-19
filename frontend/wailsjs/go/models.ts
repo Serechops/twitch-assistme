f4bd8ab78faeb0182dfaec8fde9d3b6ce83a6831
Serechops <cptwestfort@gmail.com>
2026-04-18 23:04:36 -0400

    feat: add PollChoiceDTO and PollDTO classes with conversion methods

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,5 +1,65 @@
 export namespace main {
 	
+	export class PollChoiceDTO {
+	    id: string;
+	    title: string;
+	    votes: number;
+	    channelPointsVotes: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new PollChoiceDTO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.title = source["title"];
+	        this.votes = source["votes"];
+	        this.channelPointsVotes = source["channelPointsVotes"];
+	    }
+	}
+	export class PollDTO {
+	    id: string;
+	    title: string;
+	    choices: PollChoiceDTO[];
+	    status: string;
+	    duration: number;
+	    startedAt: string;
+	    endedAt: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new PollDTO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.title = source["title"];
+	        this.choices = this.convertValues(source["choices"], PollChoiceDTO);
+	        this.status = source["status"];
+	        this.duration = source["duration"];
+	        this.startedAt = source["startedAt"];
+	        this.endedAt = source["endedAt"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
 	export class SettingsDTO {
 	    soundEnabled: boolean;
 	    soundPath: string;
